65b4328818a82fd8d7e403bbd6d03254ae3b77a7
Paul Falgout <paul@otterball.com>
2019-05-08 22:36:47 +0900

    Add datepicker component

---
+++ src/js/components/datepicker/index.js
@@ -0,0 +1,139 @@
+/*
+    Datepicker Component init API
+
+    uiView:                    // required - the view ui $('.selector') is in
+    ui: $('.selector')         // if not defined uiView will be used
+    position: {                // set by default to uiView.getBounds(ui)
+        top: 1,
+        left: 1,
+    }
+
+    state: {
+        beginDate moment(),    // No dates selectable before this date
+        endDate: moment(),     // No dates selectable after this date
+        currentMonth: moment(),
+        selectedDate: moment()
+    }
+*/
+
+import _ from 'underscore';
+
+import Radio from 'backbone.radio';
+
+import moment from 'moment';
+
+import Component from 'js/base/component';
+
+import { LayoutView, HeaderView, CalendarView } from './datepicker_views';
+
+import StateModel from './datepicker_state.js';
+
+export default Component.extend({
+  StateModel,
+
+  stateEvents: {
+    'change': 'renderView',
+    'change:selectedDate': 'onChangeSelectedDate',
+  },
+
+  onChangeSelectedDate(state, selectedDate) {
+    // proxy state event to component
+    this.triggerMethod('state:change:selectedDate', state, selectedDate);
+  },
+
+  initialize(options) {
+    this.mergeOptions(options, ['position', 'uiView', 'ui']);
+
+    this.region = this.getOption('region') || Radio.request('pop', 'get:region');
+
+    this.listenTo(this.uiView, 'render destroy', this.destroy);
+  },
+
+  ViewClass: LayoutView,
+
+  viewEvents: {
+    'click:today': 'onSelectToday',
+    'click:tomorrow': 'onSelectTomorrow',
+    'click:clear': 'onSelectClear',
+  },
+
+  onSelectToday() {
+    this.selectDate(moment());
+  },
+
+  onSelectTomorrow() {
+    this.selectDate(moment().add(1, 'days'));
+  },
+
+  onSelectClear() {
+    this.selectDate(null);
+  },
+
+  onRenderView(view) {
+    view.showChildView('header', this.getHeaderView());
+    view.showChildView('calendar', this.getCalendarView());
+  },
+
+  getHeaderView() {
+    const model = this.getState();
+
+    const headerView = new HeaderView({ model });
+
+    this.listenTo(headerView, {
+      'click:nextMonth': this.onSelectNextMonth,
+      'click:prevMonth': this.onSelectPrevMonth,
+    });
+
+    return headerView;
+  },
+
+  onSelectNextMonth() {
+    const state = this.getState();
+    state.setCurrentMonth(state.getNextMonth());
+  },
+
+  onSelectPrevMonth() {
+    const state = this.getState();
+    state.setCurrentMonth(state.getPrevMonth());
+  },
+
+  getCalendarView() {
+    const model = this.getState();
+
+    const calView = new CalendarView({
+      model,
+      collection: model.getCalendar(),
+    });
+
+    this.listenTo(calView, {
+      'select:date': this.onSelectDate,
+    });
+
+    return calView;
+  },
+
+  onSelectDate({ model }) {
+    const date = model.get('date');
+    const currentMonth = this.getState().getCurrentMonth();
+
+    this.selectDate(currentMonth.date(date));
+  },
+
+  position() {
+    return this.uiView && this.uiView.getBounds(this.ui);
+  },
+
+  showView(view) {
+    const options = _.extend({ popWidth: 256 }, _.result(this, 'position'));
+
+    this.getRegion().show(view, options);
+  },
+
+  selectDate(date) {
+    if (date) {
+      date = moment(date).startOf('day');
+    }
+
+    this.setState('selectedDate', date);
+  },
+});
