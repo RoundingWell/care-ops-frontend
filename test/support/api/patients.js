693e9778597c10e63d1b34522249bda0c5a918fa
Luke Lancaster <luke.lancaster@roundingwell.com>
2019-06-25 11:31:48 -0500

    Add routePatients to return collection of patients

--- test/support/api/patients.js
+++ test/support/api/patients.js
@@ -31,3 +31,39 @@ Cypress.Commands.add('routePatient', (mutator = _.identity) => {
   })
     .as('routePatient');
 });
+
+Cypress.Commands.add('routePatients', (mutator = _.identity) => {
+  cy
+    .fixture('collections/patients').as('fxPatients')
+    .fixture('collections/patient-fields').as('fxPatientFields')
+    .fixture('collections/groups').as('fxGroups');
+
+  cy.route({
+    url: /api\/patients/,
+    response() {
+      const data = getResource(_.sample(this.fxPatients, 30), 'patients');
+      const groups = _.sample(this.fxGroups, 2);
+
+      let included = [];
+
+      included = getIncluded(included, groups, 'groups');
+
+      _.each(data, (patient) => {
+        const fields = _.sample(this.fxPatientFields, 2);
+
+        patient.relationships = {
+          groups: { data: getRelationship(groups, 'groups') },
+          fields: { data: getRelationship(fields, 'patient-fields') },
+        };
+
+        included = getIncluded(included, fields, 'patient-fields');
+      });
+
+      return mutator({
+        data,
+        included,
+      });
+    },
+  })
+    .as('routePatients');
+});
