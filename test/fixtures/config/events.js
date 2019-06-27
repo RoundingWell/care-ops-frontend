8cbf83b4a5f21ec663d22d613f68cd7f1ce53946
Luke Lancaster <luke.lancaster@roundingwell.com>
2019-06-27 16:17:00 -0500

    Event fixture generator

---
+++ test/fixtures/config/events.js
@@ -0,0 +1,84 @@
+const _ = require('underscore');
+const faker = require('faker');
+const moment = require('moment');
+
+const { generate } = require('./clinicians');
+const roles = require('../test/roles.json');
+const states = require('../test/states.json');
+
+module.exports = {
+  generate() {
+    const types = {
+      ActionAdded() {
+        return;
+      },
+      ActionClinicianAssigned() {
+        return {
+          to: generate(),
+        };
+      },
+      ActionDetailsUpdated() {
+        return;
+      },
+      ActionDueDateUpdated() {
+        return {
+          from: faker.date.between(
+            moment.utc().subtract(1, 'week').format(),
+            moment.utc().add(1, 'week').format()
+          ),
+          to: faker.date.between(
+            moment.utc().subtract(1, 'week').format(),
+            moment.utc().add(1, 'week').format()
+          ),
+        };
+      },
+      ActionDurationUpdated() {
+        return {
+          from: faker.random.number({ min: 1, max: 1200 }),
+          to: faker.random.number({ min: 1, max: 1200 }),
+        };
+      },
+      ActionNameUpdated() {
+        return {
+          from: `${ faker.company.bsBuzz() } ${ faker.company.catchPhraseNoun() }`,
+          to: `${ faker.company.bsBuzz() } ${ faker.company.catchPhraseNoun() }`,
+        };
+      },
+      ActionRoleAssigned() {
+        return {
+          to: _.sample(roles),
+        };
+      },
+      ActionStateUpdated() {
+        return {
+          from: _.sample(states),
+          to: _.sample(states),
+        };
+      },
+    };
+
+    const editor = generate();
+    editor.role = _.sample(roles);
+
+    // Base Metadata
+    const metadata = {
+      entityId: [
+        faker.random.uuid(),
+      ],
+      editor,
+    };
+
+    const type = _.sample(_.keys(types));
+    const value = _.result(types, type);
+    if (_.isObject(value)) {
+      metadata.value = value;
+    }
+
+    return {
+      id: faker.random.uuid(),
+      type,
+      date: faker.date.past(),
+      metadata,
+    };
+  },
+};
