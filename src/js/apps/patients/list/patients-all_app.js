f0ac24c4b3e463563634669defd39bed8101d0a0
Paul Falgout <paul@otterball.com>
2019-06-29 23:54:49 +0900

    Add Patient list stylings

--- src/js/apps/patients/list/patients-all_app.js
+++ src/js/apps/patients/list/patients-all_app.js
@@ -2,13 +2,16 @@ import Radio from 'backbone.radio';
 
 import App from 'js/base/app';
 
-import { ListView } from 'js/views/patients/list/patients-all_views';
+import { ListView, LayoutView } from 'js/views/patients/list/patients-all_views';
 
 export default App.extend({
+  onBeforeStart() {
+    this.showView(new LayoutView());
+  },
   beforeStart() {
     return Radio.request('entities', 'fetch:patients:collection');
   },
   onStart(options, collection) {
-    this.showView(new ListView({ collection }));
+    this.showChildView('list', new ListView({ collection }));
   },
 });
--- src/js/views/patients/list/patients-all_views.js
+++ src/js/views/patients/list/patients-all_views.js
@@ -3,14 +3,16 @@ import Radio from 'backbone.radio';
 import hbs from 'handlebars-inline-precompile';
 import { View, CollectionView } from 'marionette';
 
+import 'sass/modules/list-pages.scss';
+import 'sass/modules/table-list.scss';
+
 const ItemView = View.extend({
+  className: 'table-list__item',
+  tagName: 'tr',
   template: hbs`
-    {{ first_name }} {{ last_name }}
-    {{#each groups}}
-      <strong>{{ this.name }}</strong>
-    {{/each}}
+    <td class="table-list__cell w-20">{{ first_name }} {{ last_name }}</td>
+    <td class="table-list__cell w-80">{{#each groups}}{{#unless @first}}, {{/unless}}{{ this.name }}{{/each}}</td>
   `,
-  className: 'patient-list__item',
   templateContext() {
     return {
       groups: _.map(this.model.getGroups().models, 'attributes'),
@@ -24,10 +26,55 @@ const ItemView = View.extend({
   },
 });
 
+const LayoutView = View.extend({
+  className: 'flex-region',
+  template: hbs`
+    <div class="list-page__header">
+      <div class="list-page__title">{{ @intl.patients.list.patientsAllViews.layoutView.title }}</div>
+      <div class="list-page__filters" data-filters-region></div>
+      <table class="w-100 js-list-header"><tr>
+        <td class="table-list__header w-20">{{ @intl.patients.list.patientsAllViews.layoutView.patientHeader }}</td>
+        <td class="table-list__header w-80">{{ @intl.patients.list.patientsAllViews.layoutView.groupHeader }}</td>
+      </tr></table>
+    </div>
+    <div class="flex-region list-page__list js-list" data-list-region></div>
+  `,
+  regions: {
+    filters: '[data-filters-region]',
+    list: '[data-list-region]',
+  },
+  childViewEvents: {
+    'update:listDom': 'fixWidth',
+  },
+  ui: {
+    listHeader: '.js-list-header',
+    list: '.js-list',
+  },
+  fixWidth() {
+    if (!this.isRendered()) return;
+
+    const headerWidth = this.ui.listHeader.width();
+    const listWidth = this.ui.list.contents().width();
+    const listPadding = parseInt(this.ui.list.css('paddingRight'), 10);
+    const scrollbarWidth = headerWidth - listWidth;
+
+    this.ui.list.css({ paddingRight: `${ listPadding - scrollbarWidth }px` });
+  },
+});
+
 const ListView = CollectionView.extend({
+  tagName: 'table',
   childView: ItemView,
+  onAttach() {
+    this.triggerMethod('update:listDom', this);
+  },
+  onRenderChildren() {
+    if (!this.isAttached()) return;
+    this.triggerMethod('update:listDom', this);
+  },
 });
 
 export {
+  LayoutView,
   ListView,
 };
