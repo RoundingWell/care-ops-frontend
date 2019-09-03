8d36ee8299e19c3e114d7d7063259a65c35e6689
Luke Lancaster <luke.lancaster@roundingwell.com>
2019-09-03 09:33:02 -0500

    Remove patched component code

--- src/js/base/component.js
+++ src/js/base/component.js
@@ -1,12 +1,3 @@
 import { Component } from 'marionette.toolkit';
 
-export default Component.extend({
-  // https://github.com/RoundingWellOS/marionette.toolkit/issues/241
-  showIn(region, viewOptions, regionOptions) {
-    this._region = region;
-
-    this.show(viewOptions, regionOptions);
-
-    return this;
-  },
-});
+export default Component;
