0d285188bd20ce9d3f0a5ffbe2c6984ad3d7fde1
Paul Falgout <paul@otterball.com>
2019-10-17 23:47:18 +0900

    Add E2E env variable for E2E testing a production build

--- config/webpack.env.js
+++ config/webpack.env.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   isCI: !!process.env.CI,
+  isE2E: !!process.env.E2E,
   isProduction: process.env.NODE_ENV === 'production',
   jsRoot: path.resolve(process.cwd(), './src/js'),
   sassRoot: path.resolve(process.cwd(), './src/sass'),
