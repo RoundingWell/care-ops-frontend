a33e9c33a35f6bff20c80770617672ebaaea2cf2
paulfalgout <paul.falgout@roundingwell.com>
2019-10-21 23:50:46 +0900

    Make programs area develop only for now

--- src/js/apps/globals/nav_app.js
+++ src/js/apps/globals/nav_app.js
@@ -109,7 +109,7 @@ export default App.extend({
   },
   onStart() {
     const currentUser = Radio.request('bootstrap', 'currentUser');
-    if (!currentUser.can('admin')) appNavMenu.remove('AdminApp');
+    if (_DEVELOP_ && !currentUser.can('admin')) appNavMenu.remove('AdminApp');
 
     // If there's only 2 options, only sign out
     if (appNavMenu.length === 2) appNavMenu.shift();
