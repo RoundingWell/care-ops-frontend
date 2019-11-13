47f0c3dd374c9471532c295bc1dafe0573a50363
paulfalgout <paul.falgout@roundingwell.com>
2019-11-13 23:05:24 +0900

    Make login determinate on authenticated
    Keep pathname as login as possible as well as it's not `/login`
    
    Also moved css to views and combined them in a login directory.  Also made sure there are no style leaks.

--- src/js/views/globals/login/login-prompt_views.js
+++ src/js/views/globals/login/login-prompt_views.js
@@ -2,15 +2,19 @@ import { View } from 'marionette';
 
 import LoginPromptTemplate from './login-prompt.hbs';
 
-import 'assets/css/login-prompt.css';
+import './login-prompt.css';
 
 const LoginPromptView = View.extend({
   el: 'body',
+  triggers: {
+    'click .js-login': 'click:login',
+  },
+  onRender() {
+    this.$el.addClass('login-prompt');
+  },
   template: LoginPromptTemplate,
-  templateContext() {
-    return {
-      url: location.host,
-    };
+  templateContext: {
+    url: location.host,
   },
 });
 
