89f4f7a85e21af071159feac1a397462d940c3f8
Luke Lancaster <luke.lancaster@roundingwell.com>
2020-01-17 10:54:43 -0600

    Add empty view to patient flow list

--- test/integration/patients/patient/patient-flow.js
+++ test/integration/patients/patient/patient-flow.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 
 const now = moment.utc();
 
-context('program flow page', function() {
+context('patient flow page', function() {
   specify('context trail', function() {
     cy
       .server()
@@ -27,5 +27,38 @@ context('program flow page', function() {
       .wait('@routeFlow')
       .wait('@routeFlowActions')
       .wait('@routePatientByFlow');
+  });
+
+  specify('empty view', function() {
+    cy
+      .server()
+      .routeFlow(fx => {
+        fx.data.id = '1';
+
+        fx.data.attributes.name = 'Test Flow';
+        fx.data.attributes.updated_at = now.format();
+
+        return fx;
+      })
+      .routeFlowActions(fx => {
+        fx.data = [];
+
+        return fx;
+      }, '1')
+      .routePatientByFlow(fx => {
+        fx.data.id = '1';
+        fx.data.attributes.first_name = 'Test';
+        fx.data.attributes.last_name = 'Patient';
+
+        return fx;
+      })
+      .visit('/flow/1')
+      .wait('@routeFlow')
+      .wait('@routeFlowActions')
+      .wait('@routePatientByFlow');
+
+    cy
+      .get('.workflows__empty-list')
+      .contains('No Actions');
   });
 });
