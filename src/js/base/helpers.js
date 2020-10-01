414c621735f79a8e846fb75b3465bb85d642514e
Paul Falgout <paul.falgout@roundingwell.com>
2020-10-02 01:02:10 +0900

    Remove unused duration

--- src/js/base/helpers.js
+++ src/js/base/helpers.js
@@ -1,6 +1,5 @@
 import _ from 'underscore';
 import 'js/utils/formatting';
-import duration from 'js/utils/duration';
 import Handlebars from 'handlebars/runtime';
 
 import './moment';
@@ -12,13 +11,5 @@ Handlebars.registerHelper({
     if (!hash.noEscape) text = Handlebars.escapeExpression(text);
 
     return new Handlebars.SafeString(_.matchText(text, query));
-  },
-});
-
-Handlebars.registerHelper({
-  formatDuration(dur, key, { hash = {} }) {
-    if (!dur) return new Handlebars.SafeString(hash.defaultHtml || '');
-
-    return duration(dur, hash.inputKey).get(key);
   },
 });
