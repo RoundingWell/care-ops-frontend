23f5ffbf8608fd2303d713baa0febff50d682bb9
Will Weaver <will+git@roundingwell.com>
2020-10-27 10:00:16 -0400

    Chore/formio style tweaks (#386)
     introducing bottstrap theme to formio

--- src/js/formapp.js
+++ src/js/formapp.js
@@ -1,7 +1,7 @@
 /* global Formio */
 import 'formiojs/dist/formio.form.min';
 import 'formiojs/dist/formio.form.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'sass/formapp/bootstrap.min.css';
 
 import $ from 'jquery';
 import _ from 'underscore';
