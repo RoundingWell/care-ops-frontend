d16f3f3ba6e1dae1d52174b90d32af493f916a0d
Lisa Manresa <lisa.manresa@roundingwell.com>
2020-12-02 14:29:41 -0800

    Create patient support

--- src/js/entities-service/entities/patients.js
+++ src/js/entities-service/entities/patients.js
@@ -1,19 +1,50 @@
-import { intersection, pluck } from 'underscore';
+import { extend, intersection, pluck } from 'underscore';
 import Radio from 'backbone.radio';
 import Store from 'backbone.store';
 import BaseCollection from 'js/base/collection';
 import BaseModel from 'js/base/model';
+
+import trim from 'js/utils/formatting/trim';
 
 const TYPE = 'patients';
 
 const _Model = BaseModel.extend({
   type: TYPE,
   urlRoot: '/api/patients',
+  defaults: {
+    _groups: [],
+  },
+  validate(attrs) {
+    if (!trim(attrs.first_name)) {
+      return 'A patient first name is required';
+    }
+
+    if (!trim(attrs.last_name)) {
+      return 'A patient last name is required';
+    }
+
+    if (!trim(attrs.birth_date)) {
+      return 'A patient date of birth is required';
+    }
+
+    if (!attrs.sex) {
+      return 'A patient sex is required';
+    }
+  },
   getGroups() {
     return Radio.request('entities', 'groups:collection', this.get('_groups'));
   },
   getFields() {
     return Radio.request('entities', 'patientFields:collection', this.get('_patient_fields'));
+  },
+  saveAll(attrs) {
+    if (this.isNew()) attrs = extend({}, this.attributes, attrs);
+
+    const relationships = {
+      'groups': this.toRelation(attrs._groups, 'groups'),
+    };
+
+    return this.save(attrs, { relationships }, { wait: true });
   },
 });
 
