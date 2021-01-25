126bcdde4d23e4316df23586aae77b5c829a42da
Lisa Manresa <lisa.manresa@roundingwell.com>
2021-01-25 09:33:08 -0800

    Schedule view date filter

--- src/js/views/patients/schedule/filters_views.js
+++ src/js/views/patients/schedule/filters_views.js
@@ -5,6 +5,7 @@ import 'sass/modules/buttons.scss';
 
 import Droplist from 'js/components/droplist';
 import OwnerDroplist from 'js/views/patients/shared/components/owner_component';
+import DateFilterComponent from 'js/views/patients/shared/components/date-filter';
 
 import './schedule.scss';
 
@@ -13,12 +14,10 @@ const FiltersView = View.extend({
   template: hbs`
     <div class="schedule__filter" data-group-filter-region></div>
     <div class="schedule__filter" data-owner-filter-region></div>
-    <div class="schedule__filter" data-date-filter-region></div>
   `,
   regions: {
     group: '[data-group-filter-region]',
     owner: '[data-owner-filter-region]',
-    date: '[data-date-filter-region]',
   },
 });
 
@@ -36,4 +35,5 @@ export {
   FiltersView,
   GroupsDropList,
   OwnerDroplist,
+  DateFilterComponent,
 };
