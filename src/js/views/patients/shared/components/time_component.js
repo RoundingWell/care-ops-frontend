c6ce3dfbb187fd7a22f29d0e86d60bb8703db6b7
Paul Falgout <paul.falgout@roundingwell.com>
2021-09-21 16:21:53 +0900

    Update sidebar styles

--- src/js/views/patients/shared/components/time_component.js
+++ src/js/views/patients/shared/components/time_component.js
@@ -23,7 +23,7 @@ const times = _times(96, function(n) {
 
 const NoTimeCompactTemplate = hbs`{{far "clock"}}`;
 
-const TimeTemplate = hbs`{{far "clock"}} {{formatDateTime id "LT" inputFormat="HH:mm:ss" defaultHtml=(intlGet "patients.shared.components.timeComponent.placeholderText")}}`;
+const TimeTemplate = hbs`{{far "clock"}}{{formatDateTime id "LT" inputFormat="HH:mm:ss" defaultHtml=(intlGet "patients.shared.components.timeComponent.placeholderText")}}`;
 
 export default Droplist.extend({
   collection: new Backbone.Collection(times),
