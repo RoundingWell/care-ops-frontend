ed8772a72fcefa8ed5ddc53a225ef246546571e8
Ben Tennant <ben.tennant@roundingwell.com>
2022-04-28 09:33:19 -0500

    Set federated logout to support SLO

--- src/js/auth.js
+++ src/js/auth.js
@@ -87,7 +87,7 @@ function login(success) {
 
 function logout() {
   localStorage.removeItem(RWELL_KEY);
-  auth0.logout({ returnTo: location.origin });
+  auth0.logout({ returnTo: location.origin, federated: true });
 }
 
 function loginWithRedirect(opts) {
