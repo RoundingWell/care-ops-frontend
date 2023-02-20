402cf9ab47c65806c63d9bbf21087ca5bf91a61d
Paul Falgout <paul.falgout@roundingwell.com>
2023-02-21 05:14:52 +0900

    Redirect legacy urls to current workspace

--- src/js/apps/globals/error_app.js
+++ src/js/apps/globals/error_app.js
@@ -1,23 +1,42 @@
-import { bind } from 'underscore';
+import { bind, contains } from 'underscore';
+import Radio from 'backbone.radio';
 
 import RouterApp from 'js/base/routerapp';
 
+import getRootRoute from 'js/utils/root-route';
+
 import { ErrorView } from 'js/views/globals/error/error_views';
+
+const legacyRoots = [
+  'worklist',
+  'patient',
+  'patient-action',
+  'flow',
+  'schedule',
+  'clinicians',
+  'dashboards',
+  'form',
+  'program',
+  'programs',
+  'program-flow',
+];
 
 export default RouterApp.extend({
   eventRoutes: {
     'notFound': {
       action: 'show404',
       route: '404',
+      root: true,
     },
     'error': {
       action: 'show500',
       route: '500',
+      root: true,
     },
   },
 
   initialize() {
-    this.router.route('*unknown', '404', bind(this.show404, this));
+    this.router.route('*unknown', '404', bind(this.handleUnknown, this));
   },
 
   viewEvents: {
@@ -28,7 +47,14 @@ export default RouterApp.extend({
     this.getRegion().empty();
   },
 
-  show404() {
+  handleUnknown() {
+    const rootRoute = getRootRoute();
+    if (contains(legacyRoots, rootRoute)) {
+      const workspace = Radio.request('bootstrap', 'currentWorkspace');
+      this.replaceUrl(`/${ workspace.get('slug') }${ location.pathname }`);
+      return;
+    }
+
     this.start();
     this.showView(new ErrorView({ is404: true }));
   },
