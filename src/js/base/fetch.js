2ce428cf9678e472bcc976b79a07f0f846a4ed1a
Paul Falgout <paul.falgout@roundingwell.com>
2023-04-19 16:30:58 +0900

    Replace $.param with rwell specific param handling
    This prevents some of the `filter[foo][]=1&filter[foo][]=2` when what we want is `filter[foo]=1,2`
    
    And it lessens our dependency on jQuery.

--- src/js/base/fetch.js
+++ src/js/base/fetch.js
@@ -1,15 +1,47 @@
 //  Similar to https://github.com/akre54/Backbone.Fetch
-import $ from 'jquery';
-import { isObject, defaults, extend } from 'underscore';
+import { isObject, isArray, defaults, extend, map, flatten, reduce, first, rest } from 'underscore';
 import Radio from 'backbone.radio';
 
 import { getToken } from 'js/auth';
+
+function getValue(value) {
+  return encodeURIComponent(value ?? '');
+}
+
+function getKey(key) {
+  // Builds key[subkey] or key[subkey1][subkey2]
+  if (isArray(key)) {
+    const firstKey = encodeURIComponent(first(key));
+
+    return reduce(rest(key), (str, k) => `${ str }[${ encodeURIComponent(k) }]`, firstKey);
+  }
+
+  return encodeURIComponent(key);
+}
+
+function buildParams(value, key) {
+  if (isArray(value)) {
+    // Builds key=value1,value2,value3
+    return `${ getKey(key) }=${ value.map(getValue).join() }`;
+  }
+
+  if (isObject(value)) {
+    // Builds key[subkey]=value or key[subkey1][subkey2]=value
+    return flatten(map(value, (val, name) => buildParams(val, flatten([key, name])))).join('&');
+  }
+
+  return `${ getKey(key) }=${ getValue(value) }`;
+}
+
+function serializeParams(obj) {
+  return map(obj, buildParams).join('&');
+}
 
 // Makes data object into `/url?param1=value1&param2=value2` string
 function getUrl(url, data) {
   if (!isObject(data)) return url;
 
-  const params = $.param(data);
+  const params = serializeParams(data);
   if (!params) return url;
 
   return `${ url }?${ params }`;
