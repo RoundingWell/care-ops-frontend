8a1e8ea54ac94f0e3a08fc125e63f050d11ac545
Paul Falgout <paul.falgout@roundingwell.com>
2023-05-01 22:53:24 +0900

    Throw all 500 errors in the fetcher
    This was previously mostly an issue in cypress for endpoints we failed to stub.  This ought to pass if we’re doing everything correctly.

--- src/js/base/fetch.js
+++ src/js/base/fetch.js
@@ -90,8 +90,7 @@ export default async(url, opts) => {
           return Promise.reject(response);
         }
 
-        // FIXME: Should be >= 500, but missing Cypress stubs cause wide failures
-        if (response.status === 500) {
+        if (response.status >= 500) {
           Radio.trigger('event-router', 'error');
           return Promise.reject(response);
         }
