ad56cc628d2a451b9c120954577c74f92853c2c4
Paul Falgout <paul.falgout@roundingwell.com>
2023-09-19 03:41:29 +0900

    Add temporary patch for draft history bug
    This patch is just for evernorth and we should remove it in a month or so

--- src/js/services/forms.js
+++ src/js/services/forms.js
@@ -1,4 +1,4 @@
-import { map, get, debounce } from 'underscore';
+import { map, get, debounce, isEmpty } from 'underscore';
 import dayjs from 'dayjs';
 import store from 'store';
 
@@ -9,6 +9,11 @@ import App from 'js/base/app';
 import { FORM_RESPONSE_STATUS } from 'js/static';
 
 import { versions } from 'js/config';
+
+/* istanbul ignore next: Temporary patch */
+function patchEvernorthHistory(submission) {
+  if (isEmpty(submission.history)) delete submission.history;
+}
 
 export default App.extend({
   startAfterInitialized: true,
@@ -151,6 +156,9 @@ export default App.extend({
       });
   },
   fetchFormStoreSubmission({ submission }) {
+    // NOTE: Remove after 2023/10/19
+    patchEvernorthHistory(submission);
+
     const channel = this.getChannel();
 
     return Promise.all([
