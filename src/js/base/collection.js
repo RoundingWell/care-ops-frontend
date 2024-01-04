4cf451a75e795e92631d5c782d77d68de31dafb9
Paul Falgout <paul.falgout@roundingwell.com>
2024-01-05 02:44:45 +0900

    Move control and abort into base fetch

--- src/js/base/collection.js
+++ src/js/base/collection.js
@@ -1,24 +1,17 @@
-import { clone, invoke, extend, map, result, get } from 'underscore';
+import { clone, invoke, extend, map, get } from 'underscore';
 import Backbone from 'backbone';
 
-import { getActiveFetcher, registerFetcher } from './control';
 import JsonApiMixin from './jsonapi-mixin';
 
 export default Backbone.Collection.extend(extend({
   fetch(options = {}) {
-    const baseUrl = options.url || result(this, 'url');
-    let fetcher = getActiveFetcher(baseUrl, options);
-
-    /* istanbul ignore if */
-    if (!fetcher) {
-      fetcher = Backbone.Collection.prototype.fetch.call(this, options);
-
-      registerFetcher(baseUrl, fetcher);
-    }
+    const fetcher = Backbone.Collection.prototype.fetch.call(this, options);
 
     // Resolve with entity if successful
     return fetcher.then(response => {
-      return this;
+      if (!response || response.ok) return this;
+
+      return response;
     });
   },
   parse(response) {
