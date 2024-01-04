2b362857bfca4960b7c70695732830e8312e976b
Paul Falgout <paul.falgout@roundingwell.com>
2024-01-05 04:50:27 +0900

    Change dates for find in list date issue
    Test was failing because “Jan 4” was “12:00” on Jan 4th.  Need to change it to a `visitOnClock` somehow.

--- src/js/utils/handle-errors.js
+++ src/js/utils/handle-errors.js
@@ -6,6 +6,7 @@ export default async function handleErrors(error) {
   if (error.response) {
     const status = error.response.status;
     const { errors } = error.responseData;
+    /* istanbul ignore next: can't get coverage on a throw */
     throw new Error(`Error Status: ${ status } - ${ JSON.stringify(errors) }`);
   }
   throw new Error(JSON.stringify(error));
