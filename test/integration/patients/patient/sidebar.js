ff7399b6a96e830fcdccbf716dc4ccf39f066b49
Nick Major <nick.major@roundingwell.com>
2024-02-01 12:21:20 -0600

    Add `.wait('@routeWorkspacePatient')` to `update patient status` patient sidebar test

--- test/integration/patients/patient/sidebar.js
+++ test/integration/patients/patient/sidebar.js
@@ -1056,6 +1056,7 @@ context('patient sidebar', function() {
         return fx;
       })
       .visit('/patient/dashboard/1')
+      .wait('@routeWorkspacePatient')
       .wait('@routePatient');
 
     cy
