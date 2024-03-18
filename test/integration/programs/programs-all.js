56314832422b591801c1f818b57d3b7769b3a84e
Paul Falgout <paul.falgout@roundingwell.com>
2024-03-18 23:23:00 +0900

    Refactor programs-all to mergejson

--- test/integration/programs/programs-all.js
+++ test/integration/programs/programs-all.js
@@ -1,36 +1,37 @@
-import _ from 'underscore';
-
 import { testTs, testTsSubtract } from 'helpers/test-timestamp';
+
+import { getProgram } from 'support/api/programs';
 
 context('program all list', function() {
   specify('display programs list', function() {
     cy
       .routePrograms(fx => {
-        fx.data = _.sample(fx.data, 3);
-
-        fx.data[0] = {
-          id: '1',
-          attributes: {
-            name: 'First in List',
-            published_at: testTs(),
-            archived_at: null,
-            updated_at: testTs(),
-          },
-        };
-
-        fx.data[1].attributes = {
-          name: 'Last in List',
-          published_at: testTs(),
-          archived_at: null,
-          updated_at: testTsSubtract(2),
-        };
-
-        fx.data[2].attributes = {
-          name: 'Second in List, Not Published',
-          published_at: null,
-          archived_at: null,
-          updated_at: testTsSubtract(1),
-        };
+        fx.data = [
+          getProgram({
+            attributes: {
+              name: 'First in List',
+              published_at: testTs(),
+              archived_at: null,
+              updated_at: testTs(),
+            },
+          }),
+          getProgram({
+            attributes: {
+              name: 'Last in List',
+              published_at: testTs(),
+              archived_at: null,
+              updated_at: testTsSubtract(2),
+            },
+          }),
+          getProgram({
+            attributes: {
+              name: 'Second in List, Not Published',
+              published_at: null,
+              archived_at: null,
+              updated_at: testTsSubtract(1),
+            },
+          }),
+        ];
 
         return fx;
       })
