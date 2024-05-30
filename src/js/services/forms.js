2eb81643e0a9e96db72a20dd9ee744781104fd13
Nick Major <nick.major@roundingwell.com>
2024-05-30 13:56:29 -0500

    Remove unused variable in forms service file

--- src/js/services/forms.js
+++ src/js/services/forms.js
@@ -103,7 +103,7 @@ export default App.extend({
     try {
       store.set(this.getStoreId(), { submission, updated });
       this.trigger('update:submission', updated);
-    } catch (e) /* istanbul ignore next: Tested locally, test runner borks on CI */ {
+    } catch /* istanbul ignore next: Tested locally, test runner borks on CI */ {
       store.each((value, key) => {
         if (String(key).startsWith('form-subm-')) store.remove(key);
       });
