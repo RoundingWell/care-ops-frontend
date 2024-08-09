ac090b4cd9fafd9d86373ee413239c7e2811caae
Nick Major <nick.major@roundingwell.com>
2024-08-09 11:34:39 -0500

    Clean up view creation and mounting

--- src/js/base/helpers.cy.js
+++ src/js/base/helpers.cy.js
@@ -1,4 +1,3 @@
-import Backbone from 'backbone';
 import { View } from 'marionette';
 
 import hbs from 'handlebars-inline-precompile';
@@ -7,73 +6,17 @@ import { testTs } from 'helpers/test-timestamp';
 import formatDate from 'helpers/format-date';
 
 context('Handlebars helpers', function() {
-  const testDate = testTs();
-
-  const MatchTextView = View.extend({
-    template: hbs`
-      <div class="test-element">
-        {{matchText "Patient Name" null}}
-      </div>
-      <div class="test-element">
-        {{matchText "Patient Name" "Patient"}}
-      </div>
-    `,
-  });
-
-  const DateTimeView = View.extend({
-    template: hbs`
-      <div class="test-element">
-        {{formatDateTime null "lll"}}
-      </div>
-      <div class="test-element">
-        {{formatDateTime testDate "lll"}}
-      </div>
-      <div class="test-element">
-        {{formatDateTime testDate "lll" nowrap=false}}
-      </div>
-      <div class="test-element">
-        {{formatDateTime null "lll" defaultHtml="No Date Available"}}
-      </div>
-    `,
-    templateContext() {
-      return {
-        testDate,
-      };
-    },
-  });
-
-  const PhoneView = View.extend({
-    template: hbs`
-      <div class="test-element">
-        {{formatPhoneNumber null}}
-      </div>
-      <div class="test-element">
-        {{formatPhoneNumber phone}}
-      </div>
-      <div class="test-element">
-        {{formatPhoneNumber badPhone}}
-      </div>
-      <div class="test-element">
-        {{formatPhoneNumber null defaultHtml="No Phone Available"}}
-      </div>
-    `,
-    templateContext() {
-      return {
-        phone: '6155555551',
-        badPhone: 'UNKNOWN',
-      };
-    },
-  });
-
-
   specify('Match text formatting', function() {
-    const matchTextView = new MatchTextView({
-      model: new Backbone.Model(),
+    const MatchTextView = View.extend({
+      template: hbs`
+        <div class="test-element">{{matchText "Patient Name" null}}</div>
+        <div class="test-element">{{matchText "Patient Name" "Patient"}}</div>
+      `,
     });
 
     cy
-      .mount(rootView => {
-        return matchTextView;
+      .mount(() => {
+        return new MatchTextView();
       })
       .as('root');
 
@@ -93,13 +36,25 @@ context('Handlebars helpers', function() {
   });
 
   specify('Date time formatting', function() {
-    const dateTimeView = new DateTimeView({
-      model: new Backbone.Model(),
+    const testDate = testTs();
+
+    const DateTimeView = View.extend({
+      template: hbs`
+        <div class="test-element">{{formatDateTime null "lll"}}</div>
+        <div class="test-element">{{formatDateTime testDate "lll"}}</div>
+        <div class="test-element">{{formatDateTime testDate "lll" nowrap=false}}</div>
+        <div class="test-element">{{formatDateTime null "lll" defaultHtml="No Date Available"}}</div>
+      `,
+      templateContext() {
+        return {
+          testDate,
+        };
+      },
     });
 
     cy
-      .mount(rootView => {
-        return dateTimeView;
+      .mount(() => {
+        return new DateTimeView();
       })
       .as('root');
 
@@ -132,13 +87,24 @@ context('Handlebars helpers', function() {
   });
 
   specify('Phone number formatting', function() {
-    const phoneView = new PhoneView({
-      model: new Backbone.Model(),
+    const PhoneView = View.extend({
+      template: hbs`
+        <div class="test-element">{{formatPhoneNumber null}}</div>
+        <div class="test-element">{{formatPhoneNumber phone}}</div>
+        <div class="test-element">{{formatPhoneNumber badPhone}}</div>
+        <div class="test-element">{{formatPhoneNumber null defaultHtml="No Phone Available"}}</div>
+      `,
+      templateContext() {
+        return {
+          phone: '6155555551',
+          badPhone: 'UNKNOWN',
+        };
+      },
     });
 
     cy
-      .mount(rootView => {
-        return phoneView;
+      .mount(() => {
+        return new PhoneView();
       })
       .as('root');
 
