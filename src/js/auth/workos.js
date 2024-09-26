40426611f63192c4cce1f7e225a3500c1de8994f
Paul Falgout <paul.falgout@roundingwell.com>
2024-09-27 00:54:36 +0900

    WIP WorkOS implementation

--- src/js/auth/workos.js
+++ src/js/auth/workos.js
@@ -1,28 +1,111 @@
-import { isEmpty } from 'underscore';
+import { extend, isEmpty } from 'underscore';
 
-import { /*  workosConfig as config, */ appConfig } from 'js/config';
+import { createClient } from '@workos-inc/authkit-js';
 
-import { PATH_LOGOUT } from '.config';
+import { workosConfig as config, appConfig } from 'js/config';
+
+import { LoginPromptView } from 'js/views/globals/prelogin/prelogin_views';
+
+import { PATH_ROOT, PATH_RWELL, PATH_AUTHD, PATH_LOGIN, PATH_LOGOUT } from '.config';
 
 let token;
+let authkit;
 
-// Sets a token when not using auth0;
 function setToken(tokenString) {
   token = tokenString;
 }
 
 function getToken() {
   if (token) return token;
-  // if (!auth0 || !navigator.onLine) return;
+  if (!authkit || !navigator.onLine) return;
 
-  // return auth0
-  //   .getTokenSilently()
-  //   .catch(() => {
-  //     logout();
-  //   });
+  return authkit
+    .getAccessToken()
+    .catch(() => {
+      logout();
+    });
 }
 
-function auth(success) {
+/*
+ * Modifies the current history state
+ */
+function replaceState(state) {
+  window.history.replaceState({}, document.title, state);
+}
+
+function login(state = PATH_ROOT) {
+  // iframe buster
+  if (top !== self) {
+    top.location = PATH_LOGIN;
+    return;
+  }
+
+  if (appConfig.disableLoginPrompt) {
+    authkit.signIn({ state });
+    return;
+  }
+
+  replaceState(PATH_LOGIN);
+
+  const loginPromptView = new LoginPromptView();
+
+  loginPromptView.on('click:login', ()=> {
+    authkit.signIn({ state });
+  });
+
+  loginPromptView.render();
+}
+
+async function createAuthkit(success, pathName) {
+  const isRW = pathName === PATH_RWELL || localStorage.getItem(PATH_RWELL);
+
+  const createClientOptions = {
+    redirectUri: location.origin + PATH_AUTHD,
+    onRedirectCallback: ({ user, state }) => {
+      if (!user) {
+        login(state);
+        return;
+      }
+
+      if (state === PATH_LOGIN) state = PATH_ROOT;
+
+      if (state === PATH_RWELL) {
+        state = PATH_ROOT;
+        localStorage.setItem(PATH_RWELL, 1);
+      }
+
+      replaceState(state);
+
+      success();
+    },
+  };
+
+  return createClient(isRW ? config.rwClientId : config.clientId, extend(createClientOptions, config.createClientOptions));
+}
+
+/*
+ * Requests authorization
+ * And authenticates authorization if redirected to PATH_AUTHD
+ */
+
+async function auth(success) {
+  // NOTE: Set path before await create to avoid redirect replaceState changing the value
+  const pathName = location.pathname;
+
+  authkit = await createAuthkit(success, pathName);
+
+  if (pathName === PATH_AUTHD) return;
+
+  if (pathName === PATH_LOGOUT) {
+    authkit.signOut();
+    return;
+  }
+
+  if (!await authkit.getUser()) {
+    login(pathName);
+    return;
+  }
+
   success();
 }
 
